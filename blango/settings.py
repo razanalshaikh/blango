0daeffaaa97cf63f6408f9627e8ba8dcbc36bb87
Razan Alshaikh <razanalshaikh2000@gmail.com>
2026-05-14 16:24:08 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -59,12 +59,26 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'django.contrib.sites',
         "crispy_forms",
         "crispy_bootstrap5",
-
+        "allauth",
+        "allauth.account",
+        "allauth.socialaccount",
+        "allauth.socialaccount.providers.google",
     ]
+
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+    
+    # SITE_ID = 1 # for www.example.com
+    SITE_ID = 2
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+    LOGIN_REDIRECT_URL = '/'
 
     MIDDLEWARE = [
         "debug_toolbar.middleware.DebugToolbarMiddleware",#DjDT
